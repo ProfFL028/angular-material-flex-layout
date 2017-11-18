78e92d5cd7468df4ccbd3e94d5297cbdd8895131
proffl <157150278@qq.com>
2017-11-18 20:07:09 +0800

    add dishes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule, MatButtonModule, MatChipsModule, MatDialogModule} from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatChipsModule, MatListModule, MatLineModule } from '@angular/material';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { NgModule, Inject } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatBasicComponent } from './mat-basic/mat-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MatBasicComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +30,9 @@ import 'hammerjs';
     MatChipsModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
-    MatDialogModule,
-    MatIconModule,
-    Inject
+    MatListModule,
+    MatLineModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mat-basic/mat-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-basic',
+  templateUrl: './mat-basic.component.html',
+  styleUrls: ['./mat-basic.component.scss']
+})
+export class MatBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
