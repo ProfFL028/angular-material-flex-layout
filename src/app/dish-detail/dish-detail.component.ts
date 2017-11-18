abd1a2d9b6147d548e56db98aea02cc8ea643ab3
proffl <157150278@qq.com>
2017-11-18 22:09:58 +0800

    one-way bind, sending data to submodel

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,49 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
-import { Dish } from '../shared/dish';
-import { Comment } from '../shared/comment';
+import {Dish} from '../shared/dish';
 
-const DISH = {
-  name: 'Uthappizza',
-  image: '/assets/images/uthappizza.png',
-  category: 'mains',
-  label: 'Hot',
-  price: '4.99',
-  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, ' +
-  'topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-  comments: [
-    {
-      rating: 5,
-      comment: 'Imagine all the eatables, living in conFusion!',
-      author: 'John Lemon',
-      date: '2012-10-16T17:57:28.556094Z'
-    },
-    {
-      rating: 4,
-      comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-      author: 'Paul McVites',
-      date: '2014-09-05T17:57:28.556094Z'
-    },
-    {
-      rating: 3,
-      comment: 'Eat it, just eat it!',
-      author: 'Michael Jaikishan',
-      date: '2015-02-13T17:57:28.556094Z'
-    },
-    {
-      rating: 4,
-      comment: 'Ultimate, Reaching for the stars!',
-      author: 'Ringo Starry',
-      date: '2013-12-02T17:57:28.556094Z'
-    },
-    {
-      rating: 2,
-      comment: 'It\'s your birthday, we\'re gonna party!',
-      author: '25 Cent',
-      date: '2011-12-02T17:57:28.556094Z'
-    }
-  ]
-};
 
 @Component({
   selector: 'app-dish-detail',
@@ -52,9 +10,11 @@ const DISH = {
 })
 export class DishDetailComponent implements OnInit {
 
-  selectedDish: Dish = DISH;
+  @Input()
+  dish: Dish;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
