2a1ef8ae9bd31b8826351673c37d6578c62d7721
proffl <157150278@.qq.com>
2017-11-27 20:21:29 +0800

    fullfil HomeComponent and ContactUsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
+import { PromotionService} from './services/promotion.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
 
@@ -54,7 +55,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatIconModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
